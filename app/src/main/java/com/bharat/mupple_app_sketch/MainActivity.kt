fb84e4e71c7169e64bdc528744aac2e0177f1eef
bharat2005 <bharatvdeshm2005@gmail.com>
2025-11-13 23:30:44 +0530

    created abother mupple_sketch_app from start

---
+++ app/src/main/java/com/bharat/mupple_app_sketch/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.bharat.mupple_app_sketch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bharat.mupple_app_sketch.ui.theme.Mupple_app_sketchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Mupple_app_sketchTheme {
+
+
+            }
+        }
+    }
+}
+
